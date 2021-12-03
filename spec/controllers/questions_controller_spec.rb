03ef4f4f2dc6b5f7387c7b4032ed81c19ee6d7cd
Alex808r <fromxlam@gmail.com>
2021-12-03 17:14:15 +0300

    Test to method new QuestionController

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -27,4 +27,16 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
