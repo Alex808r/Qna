352c28d577129dc68bfbb1f01d6f40f54494cbf1
Alex808r <fromxlam@gmail.com>
2021-12-03 17:03:05 +0300

    Test to method show QuestionController

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    let(:questions){create_list(:question, 3)}
 
+    before { get :index }
 
-  describe '#GET index' do
-  let(:questions){create_list(:question, 3)}
-  before { get :index }
-   
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'render index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question){create(:question)}
+    before { get :show, params: {id: question} } # эквивалент get :show, params: {id: question.id}
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
