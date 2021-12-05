094caacbd9a1e9bd71ed13571e7fb9271d491f44
Alex808r <fromxlam@gmail.com>
2021-12-05 22:21:51 +0300

    Test method new AnswerController

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 class AnswersController < ApplicationController
 
   def show
     @question = Question.find(params[:question_id])
     @answer = Answer.find(params[:id])
   end
+
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new
+  end
+
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -18,7 +18,18 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :show
     end
   end
-  
 
+    describe 'GET #new' do
+      before { get :new, params: { question_id: question } }
+
+      it 'assigns a new Answer to @answer' do
+        expect(assigns(:answer)).to be_a(Answer)
+      end
+
+      it 'render new view' do
+        expect(response).to render_template :new
+      end
+
+    end
 
 end
