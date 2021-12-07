9ef447a5471e2921061bb00929cc467e48e30356
Alex808r <fromxlam@gmail.com>
2021-12-07 16:20:13 +0300

    Create FeatureHelpers. Refactor acceptance test question create

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -8,34 +8,29 @@ feature 'User can create question', %q{
   I'd like to be able to ask the question
 } do
   
-  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
-  
-  scenario 'Authenticated user asks a question' do
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+  given(:user) { create(:user) }
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit questions_path
+      click_on 'Ask question'
+    end
     
-    visit questions_path
-    click_on 'Ask question'
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'Test question body'
-    click_on 'Ask'
-    expect(page).to have_content 'Your question successfully created.'
-    expect(page).to have_content 'Test question'
-    expect(page).to have_content 'Test question body'
-  end
-  
-  scenario 'Authenticated user asks a question with errors' do
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
-
-    visit questions_path
-    click_on 'Ask question'
-    click_on 'Ask'
-    expect(page).to have_content "Title can't be blank"
+    scenario ' asks a question' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'Test question body'
+      click_on 'Ask'
+      
+      expect(page).to have_content 'Your question successfully created.'
+      expect(page).to have_content 'Test question'
+      expect(page).to have_content 'Test question body'
+    end
+    
+    scenario ' asks a question with errors' do
+      click_on 'Ask'
+      
+      expect(page).to have_content "Title can't be blank"
+    end
   end
   
   scenario 'Unauthenticated user tries to ask a question' do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers, type: :controller
+  config.include FeatureHelpers, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
