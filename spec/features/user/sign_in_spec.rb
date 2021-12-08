406ce2659c3c666a68d53ffd11c94e10b4894ad5
Alex808r <fromxlam@gmail.com>
2021-12-08 11:05:53 +0300

    Acceptance test logout user

--- spec/features/user/sign_in_spec.rb
+++ spec/features/user/sign_in_spec.rb
@@ -13,7 +13,7 @@ feature 'User can sign in', %q{
   scenario 'Registered user tries to sign in' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Log in'
+    click_button 'Log in'
     # save_and_open_page_wsl
     expect(page).to have_content 'Signed in successfully.'
   end
@@ -21,7 +21,7 @@ feature 'User can sign in', %q{
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@email.com'
     fill_in 'Password', with: '00000000'
-    click_on 'Log in'
+    click_button 'Log in'
     expect(page).to have_content 'Invalid Email or password.'
   end
 end
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -5,6 +5,6 @@ module FeatureHelpers
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Log in'
+    click_button 'Log in'
   end
 end
