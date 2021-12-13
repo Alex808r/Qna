2dbbfda499984e0066b30fa75a980c1e79376cfb
Alex808r <fromxlam@gmail.com>
2021-12-13 19:51:01 +0300

    Create migration add references best answer to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
+  belongs_to :best_answer, class_name: 'Answer', optional: true
 
   validates :title, :body, presence: true
 end
