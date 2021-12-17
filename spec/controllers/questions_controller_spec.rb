f6a4e55eae439cb5a22e695054b7d20ef3d2258c
Alex808r <fromxlam@gmail.com>
2021-12-17 16:36:19 +0300

    Test assigns the new answer action show question controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -30,6 +30,10 @@ RSpec.describe QuestionsController, type: :controller do
     it 'renders show view' do
       expect(response).to render_template :show
     end
+
+    it 'assigns the new answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
   end
 
   describe 'GET #new' do
