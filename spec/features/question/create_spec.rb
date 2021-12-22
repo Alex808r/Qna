f355aed2277b90cc0d37498c2ac79e0a39a52b51
Alex808r <fromxlam@gmail.com>
2021-12-22 12:58:42 +0300

    Feature test question create to attache file

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -30,6 +30,16 @@ feature 'User can create question', %q{
 
       expect(page).to have_content "Title can't be blank"
     end
+
+    scenario 'asks a question with attached file' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'Test question body'
+      
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      click_on 'Ask'
+
+      expect(page).to have_link? 'rails_helper.rb'
+    end
   end
 
   describe 'Unauthenticated user' do
