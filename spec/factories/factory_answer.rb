9ef13583e506db6fc61f7b37678cf2b6de1c3523
Alex808r <fromxlam@gmail.com>
2021-12-25 15:38:42 +0300

    add file to answer factory

--- spec/factories/factory_answer.rb
+++ spec/factories/factory_answer.rb
@@ -13,5 +13,13 @@ FactoryBot.define do
     trait :invalid do
       title { nil }
     end
+
+    trait :with_file do
+      file_path = "#{Rails.root}/spec/rails_helper.rb"
+  
+      after :create do |answer|
+        answer.answer_files.attach(io: File.open(file_path), filename: 'rails_helper.rb')
+      end
+    end
   end
 end
