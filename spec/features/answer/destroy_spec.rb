e569e3244f81173669e0aff9a2d8e10c13a330c2
Alex808r <fromxlam@gmail.com>
2021-12-25 16:27:46 +0300

    Feature test destroy attached file from the answer. Green.

--- spec/features/answer/destroy_spec.rb
+++ spec/features/answer/destroy_spec.rb
@@ -10,28 +10,54 @@ feature 'User can delete answer', %q{
   given(:user) { create(:user) }
   given(:not_author) { create(:user) }
   given(:question) { create(:question_factory, user: user) }
-  given!(:answer) { create(:answer, question: question, user: user) }
+  # given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:answer_with_file) { create(:answer, :with_file, question: question, user: user) }
 
-  scenario 'Author delete his answer', js: true do
-    sign_in(answer.user)
-    visit question_path(answer.question)
-    expect(page).to have_content answer.body
+  describe 'Authenticated user' do
+    context 'Author answer' do
+      background do
+        sign_in(user)
+        visit question_path(question)
+      end
 
-    click_on 'Delete answer'
-    # expect(page).to have_content 'Your answer successfully deleted'
-    expect(page).to_not have_content(answer.body)
+      scenario 'delete his answer', js: true do
+        expect(page).to have_content answer_with_file.body
+        click_on 'Delete answer'
+        # expect(page).to have_content 'Your answer successfully deleted'
+        expect(page).to_not have_content(answer_with_file.body)
+      end
+
+      scenario 'delete attahce files', js: true do
+        expect(page).to have_content answer_with_file.body
+        within '.answers' do
+          expect(page).to have_link file_name(answer_with_file)
+          click_on 'Delete file'
+
+          expect(page).to_not have_link file_name(answer_with_file)
+          expect(page).to_not have_link 'Delete file'
+        end
+      end
+    end
+
+    context 'Not author' do
+      scenario 'can not delete answer', js: true do
+        sign_in(not_author)
+        visit question_path(question)
+
+        expect(page).to_not have_link 'Delete answer'
+      end
+    end
   end
 
-  scenario 'Not author can not delete answer', js: true do
-    sign_in(not_author)
-    visit question_path(question)
+  describe 'Unauthenticated user' do
+    scenario 'can not delete answer', js: true do
+      visit question_path(question)
 
-    expect(page).to_not have_link 'Delete answer'
+      expect(page).to_not have_link 'Delete answer'
+    end
   end
 
-  scenario 'Unauthenticated user can not delete answer', js: true do
-    visit question_path(question)
-
-    expect(page).to_not have_link 'Delete answer'
+  def file_name(item)
+    item.answer_files.first.filename.to_s
   end
 end
