7216cd4e8b4ef0eab19616d85837424c0bfb9b16
Alex808r <fromxlam@gmail.com>
2022-02-11 17:04:43 +0300

    Add service object FindForOauthService

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,66 +32,12 @@ RSpec.describe User, type: :model do
   describe '.find_for_oauth' do
     let!(:user) { create(:user) }
     let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123456') }
+    let(:service) { double('Services::FindForOauth') }
 
-    context 'user already has authorization' do
-      it 'returns the user' do
-        user.authorizations.create(provider: 'github', uid: '123456')
-        expect(User.find_for_oauth(auth)).to eq user
-      end
-    end
-
-    context 'user has not authorization' do
-      context 'user already exist' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123456', info: { email: user.email }) }
-
-        it 'does not create new user' do
-          expect { User.find_for_oauth(auth) }.to_not change(User, :count)
-        end
-
-        it 'creates authorization for user' do
-          expect { User.find_for_auth(auth).to change(user.aithorizations, :count).by(1) }
-        end
-
-        it 'creates authorization with provider and uid' do
-          authorization = User.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-
-        it 'returns the user' do
-          expect(User.find_for_oauth(auth)).to eq user
-        end
-      end
-
-      context 'user does not exist' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123456', info: { email: 'new@user.com' }) }
-
-        it 'creates new user' do
-          expect { User.find_for_oauth(auth) }.to change(User, :count).by(1)
-        end
-
-        it 'returns new user' do
-          expect(User.find_for_oauth(auth)).to be_a(User)
-        end
-
-        it 'fills user email' do
-          user = User.find_for_oauth(auth)
-          expect(user.email).to eq auth.info.email
-        end
-
-        it 'creates authorization for user' do
-          user = User.find_for_oauth(auth)
-          expect(user.authorizations).to_not be_empty
-        end
-
-        it 'creates authorization with provider and uid' do
-          authorization = User.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-      end
+    it 'calls Services::FindForOauth' do
+      expect(FindForOauthService).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      User.find_for_oauth(auth)
     end
   end
 end
