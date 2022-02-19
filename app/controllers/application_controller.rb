d88fff9703a94ff0aee4c5bd3489c41b27fd8be6
Alex808r <fromxlam@gmail.com>
2022-02-19 12:55:16 +0300

    Fix rescue_from to ApplicationController(html, json, js)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@
 
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.html { redirect_to root_url, alert: exception.message }
+      format.json { render json: exception.message, status: :forbidden }
+      format.js { render status: :forbidden }
+    end
   end
 
   # check_authorization
