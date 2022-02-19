857d405581160209b3014878d5057f501bdc9c85
Alex808r <fromxlam@gmail.com>
2022-02-19 13:02:13 +0300

    Fix ability. Compare by user_id with user.id - it solves n+1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,15 +27,15 @@ class Ability
   def user_abilities
     guest_abilities
     can %i[create create_comment], [Question, Answer, Comment]
-    can :update, [Question, Answer], user: user
-    can :best_answer, Answer, question: { user: user }
+    can :update, [Question, Answer], user_id: user.id
+    can :best_answer, Answer, question: { user_id: user.id }
 
     can %i[vote_up vote_down vote_cancel], [Question, Answer] do |votable_object|
-      votable_object.user != user
+      !user.author?(votable_object)
     end
 
-    can :destroy, [Question, Answer], user: user
-    can :destroy, Link, linkable: { user: user }
+    can :destroy, [Question, Answer], user_id: user.id
+    can :destroy, Link, linkable: { user_id: user.id }
     can :destroy, ActiveStorage::Attachment do |attachment|
       user.author?(attachment.record)
     end
