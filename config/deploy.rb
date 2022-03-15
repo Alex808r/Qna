fc8a258e74766a0ec861c62209a5bb98eee4b584
Alex808r <fromxlam@gmail.com>
2022-03-15 17:40:21 +0300

    Config capistrano. App deploy

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.17.0"
+
+set :application, "qna"
+set :repo_url, "git@github.com:Alex808r/Qna.git"
+set :branch, "main"
+    # Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key", "config/credentials/production.key", "db/seeds.rb" , ".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+
+set :pty, false
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
