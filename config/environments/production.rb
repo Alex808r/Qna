571c7afa93076e369f2f6b731f20be95b4a70fdb
Alex808r <fromxlam@gmail.com>
2022-03-15 15:01:52 +0300

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
     port:                 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    password:             Rails.application.credentials.gmail[:password],
     authentication:       'plain',
     enable_starttls_auto: true }
   
